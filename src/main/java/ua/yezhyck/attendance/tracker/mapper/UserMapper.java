31b8c94239620c8ac88cde9f46e853045ba61cab
Yezhyck <sergeysergeevytch@gmail.com>
2023-07-19 00:04:18 +0300

    Add user mapper

---
+++ src/main/java/ua/yezhyck/attendance/tracker/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package ua.yezhyck.attendance.tracker.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import ua.yezhyck.attendance.tracker.dto.UserDto;
+import ua.yezhyck.attendance.tracker.dto.UserEditableDto;
+import ua.yezhyck.attendance.tracker.entity.User;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = StudyClassMapper.class)
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(source = "studyClasses", target = "studyClassDtoList")
+    UserDto mapToUserDto(User user);
+
+    @Mapping(source = "studyClassDtoList", target = "studyClasses")
+    User mapToUser(UserDto userDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "studyClasses", ignore = true)
+    User mapToUser(UserEditableDto userEditableDto);
+
+    List<UserDto> mapAllToUserDtoList(List<User> users);
+
+    List<User> mapAllToUsers(List<UserDto> userDtoList);
+}
