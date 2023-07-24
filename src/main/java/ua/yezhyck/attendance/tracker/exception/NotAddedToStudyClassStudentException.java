110414f01339f1ef5cca6a0f5d93b5663400af1d
Yezhyck <sergeysergeevytch@gmail.com>
2023-07-24 19:42:53 +0300

    Add NotAddedToStudyClassStudentException.java

---
+++ src/main/java/ua/yezhyck/attendance/tracker/exception/NotAddedToStudyClassStudentException.java
@@ -0,0 +1,8 @@
+package ua.yezhyck.attendance.tracker.exception;
+
+public class NotAddedToStudyClassStudentException extends Exception {
+
+    public NotAddedToStudyClassStudentException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ua/yezhyck/attendance/tracker/exception/handler/NotAddedToStudyClassStudentExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package ua.yezhyck.attendance.tracker.exception.handler;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class NotAddedToStudyClassStudentExceptionHandler {
-}
